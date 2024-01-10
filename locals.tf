6aef4a894b3061079959553f90fe3da4ba73b9b8
ryan-wsl <ryanef39+abstract_packet@gmai.com>
2024-01-09 22:22:23 -0500

    workspace updates

--- locals.tf
+++ locals.tf
@@ -11,7 +11,7 @@ locals {
             "firstworkspace" = {
                 description = "firstworkspace"
                 organization_name = var.organization_name
-                execution_mode = "remote"
+                execution_mode = "local"
                 project_id = module.project["firstproject"].id
                 vcs_repo_identifier = "mkdirrepo/terraform-starter"
         }
