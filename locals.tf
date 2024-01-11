4605de290663cedab27cdf619c0b6c3f3c03bec1
ryan-wsl <ryanef39+abstract_packet@gmai.com>
2024-01-10 20:27:51 -0500

    Add aws networking workspace

--- locals.tf
+++ locals.tf
@@ -22,7 +22,13 @@ locals {
             vcs_repo_identifier = "${var.github_organization_name}/terraform-github"
             organization_name = var.organization_name
     }
-      
+              "terraform-aws-networking" = {
+            description = "Automation workspace GitHub "
+            execution_mode = "remote"
+            project_id = module.project["firstproject"].id
+            vcs_repo_identifier = "${var.github_organization_name}/terraform-aws-networking"
+            organization_name = var.organization_name
+    }
     }
 
 
